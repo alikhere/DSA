227bc050e019cedc7e6979204a5365bd550ad8a3
Ali Khurshid <107321288+alikhere@users.noreply.github.com>
2024-12-04 18:46:08 +0530

    Create Array  Leaders.cpp

---
+++ Arrays&Hashing/Array  Leaders.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        int maxi = INT_MIN;
+        for(int i = n-1; i >= 0; i--) {
+            maxi = max(maxi, arr[i]);
+            
+            if(arr[i] >= maxi)
+                ans.push_back(arr[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
