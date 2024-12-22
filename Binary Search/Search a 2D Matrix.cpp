529e64511bcc233a7a5ef53d62fa3b83c5713ea3
Ali Khurshid <107321288+alikhere@users.noreply.github.com>
2024-12-23 04:10:57 +0530

    Create Search a 2D Matrix.cpp

---
+++ Binary Search/Search a 2D Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int l = 0, u = row*col - 1, mid;
+        while(l <= u) {
+            mid = (l + u)/2;//cal mid idx
+            if(matrix[mid/col][mid%col] == target)
+                return true;
+            else {
+                if(target > matrix[mid/col][mid%col])
+                    l = mid + 1;
+                else
+                    u = mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
