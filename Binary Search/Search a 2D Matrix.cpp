d32760893ea73120db93a847e3dc27d64624477e
Ali Khurshid <107321288+alikhere@users.noreply.github.com>
2025-01-01 23:59:24 +0530

    Update Search a 2D Matrix.cpp

--- Binary Search/Search a 2D Matrix.cpp
+++ Binary Search/Search a 2D Matrix.cpp
@@ -3,7 +3,7 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-        int l = 0, u = row*col - 1, mid;
+        int l = 0, u = row*col - 1, mid;//
         while(l <= u) {
             mid = (l + u)/2;//cal mid idx
             if(matrix[mid/col][mid%col] == target)
