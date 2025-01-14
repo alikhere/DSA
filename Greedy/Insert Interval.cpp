e036bd139b823a60338886e1fc974ba4d9f99825
Ali Khurshid <107321288+alikhere@users.noreply.github.com>
2025-01-15 05:09:40 +0530

    Update Insert Interval.cpp

--- Greedy/Insert Interval.cpp
+++ Greedy/Insert Interval.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {//
         vector<vector<int>> result;
         
         int i = 0;
