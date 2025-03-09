fb18fd13ba84c85bca89ca8375c7e9c2a2719766
Md Khurshid <107321288+alikhere@users.noreply.github.com>
2025-03-09 23:49:09 +0530

    Create Design HashSet.cpp

---
+++ Arrays&Hashing/Design HashSet.cpp
@@ -0,0 +1,25 @@
+class MyHashSet {
+    set<int> arr;
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        arr.insert(key);
+        
+    }
+    
+    void remove(int key) {
+        if(arr.count(key) >0)
+            arr.erase(key);
+        
+    }
+    
+    bool contains(int key) {
+        if(arr.count(key) > 0)
+            return true;
+        else return false;
+        
+    }
+};
