f6c58cb9a17ac3b073d6a46e8ac290752a3ad707
Md Khurshid <107321288+alikhere@users.noreply.github.com>
2025-03-17 00:00:15 +0530

    Update Assign Cookies.cpp

--- Greedy/Assign Cookies.cpp
+++ Greedy/Assign Cookies.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
          sort(s.begin(), s.end());n
-        sort(g.begin(), g.end());/
+        sort(g.begin(), g.end());
         int count = 0;
-        int ptr1 = 0; // pointer for s////
+        int ptr1 = 0; // pointer for s
         int ptr2 = 0; // pointer for g
         while(ptr1<s.size() && ptr2<g.size()){//
             if(s[ptr1]>=g[ptr2]){
