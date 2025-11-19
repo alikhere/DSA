8b6cb5a9d41464bbe2b7afe6f28c3147f6b806e4
Md Khurshid <107321288+alikhere@users.noreply.github.com>
2025-11-19 23:59:19 +0530

    Create Add Two Numbers.cpp

---
+++ LinkedList/Add Two Numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int num1 = 0;
+        int num2 = 0;
+        int carry = 0;
+        int sum = 0;
+        int value;
+        ListNode *head = NULL;
+        ListNode *prev = NULL;
+
+        while(l1 != NULL || l2 != NULL || carry != 0) {
+            num1 = (l1 != nullptr) ? l1->val : 0;
+            num2 = (l2 != nullptr) ? l2->val : 0;
+
+            sum = num1 + num2 + carry;
+            value = sum % 10;
+            carry = sum / 10;
+
+            ListNode  *temp = new ListNode(value);
+            if(head == NULL) {
+                head = temp;
+                prev = head;
+            } else {
+                prev->next = temp;
+                prev = temp;
+            }
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+        return head;
+    }
+};
